68d02361deed5add42e2c1712353aacd696137a0
Vrushal <ingalevrushal@gmail.com>
2023-09-08 21:46:47 +0530

    Initial Commit Automation End To End

---
+++ src/test/java/org/AutomationExamples/RestAssuredTest.java
@@ -0,0 +1,11 @@
+package org.AutomationExamples;
+
+import org.testng.annotations.Test;
+
+public class RestAssuredTest {
+
+	@Test
+	public void checkRestAssuredAPI() {
+		System.out.println("In Testing of : Rest Assured API");
+	}
+}
---
+++ src/test/java/org/AutomationExamples/SeleniumTest.java
@@ -0,0 +1,10 @@
+package org.AutomationExamples;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	@Test
+	public void checkSeleniumWebDriver() {
+		System.out.println("In Testing of : Selenium WebDriver");
+	}
+}
