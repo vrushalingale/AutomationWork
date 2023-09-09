7d1a6a90c9bfb2ec0d67b5ee046795c24a63320d
Vrushal <ingalevrushal@gmail.com>
2023-09-09 11:40:23 +0530

    adding maven code of profile to test in pipeline

--- src/test/java/org/AutomationExamples/RestAssuredTest.java
+++ src/test/java/org/AutomationExamples/RestAssuredTest.java
@@ -1,11 +1,17 @@
 package org.AutomationExamples;
 
 import org.testng.annotations.Test;
+import org.testng.annotations.Test;
 
 public class RestAssuredTest {
 
 	@Test
 	public void checkRestAssuredAPI() {
-		System.out.println("In Testing of : Rest Assured API");
+		System.out.println("In Testing of : 1 Rest Assured API");
+	}
+	
+	@Test
+	public void checkRestAssuredAgainAPI() {
+		System.out.println("In Testing of : 2 Rest Assured API");
 	}
 }
