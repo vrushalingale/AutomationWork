ef043f79cf007c1ea7de02a8afdd31b8d93a913b
Vrushal <ingalevrushal@gmail.com>
2023-09-12 23:34:21 +0530

    Learning Selenium started, pushing and continuing on new branch for selenium

---
+++ src/test/java/org/LearningMaven/RestAssuredTest.java
@@ -0,0 +1,17 @@
+package org.LearningMaven;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class RestAssuredTest {
+
+	@Test
+	public void checkRestAssuredAPI() {
+		System.out.println("In Testing of : 1 Rest Assured API");
+	}
+	
+	@Test
+	public void checkRestAssuredAgainAPI() {
+		System.out.println("In Testing of : 2 Rest Assured API");
+	}
+}
